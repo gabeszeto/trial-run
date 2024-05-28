2b497b023ff40a1e52de8b3ffb2bd43a6c81de6f
gabeszeto <gabriel.szeto@gmail.com>
2024-05-28 17:16:45 +0100

    change port railway

--- back/server.py
+++ back/server.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-
 CORS(app)
 
 @app.route("/names")
 def members():
-    return{"names": ["Alien", "Child", "Bo", "Charlie"]} 
-
+    return {"names": ["Alien", "Child", "Bo", "Charlie"]}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
