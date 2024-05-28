879692e1f281f75600575ed3af91d87836fb98a3
gabeszeto <gabriel.szeto@gmail.com>
2024-05-28 14:11:28 +0100

    add backend

---
+++ back/server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route("/names")
+def members():
+    return{"names": ["Alien", "Child", "Bo"]} 
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/names").then(
+      res => res.json()
+    ).then (
+      data => {
+        setData(data)
+        console.log(data);
+      }
+    )
+  }, [])
+
   return (
-    <div>App</div>
+    <div>
+
+      {(typeof data.names === 'undefined') ? (
+        <p>Loading</p>
+      ) : (
+        data.names.map((name, i) => (
+          <p key={i}>{name}</p>
+        ))
+      )}
+      
+    </div>
   )
 }
 
